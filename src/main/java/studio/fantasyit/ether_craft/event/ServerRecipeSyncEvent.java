0442fc6883d16907215f943cc421973488d427a4
xypp <2952795729@qq.com>
2026-05-20 03:49:54 +0800

    feat: sync extra recipes to client after datapack reload

--- src/main/java/studio/fantasyit/ether_craft/event/ServerRecipeSyncEvent.java
+++ src/main/java/studio/fantasyit/ether_craft/event/ServerRecipeSyncEvent.java
@@ -1,9 +1,12 @@
 package studio.fantasyit.ether_craft.event;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.OnDatapackSyncEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 import studio.fantasyit.ether_craft.factory.EtherProcessRecipeManager;
+import studio.fantasyit.ether_craft.network.s2c.SyncExtraRecipesS2C;
 import studio.fantasyit.ether_craft.register.RecipeTypeRegistry;
 
 @EventBusSubscriber
@@ -13,5 +16,9 @@ public class ServerRecipeSyncEvent {
         event.sendRecipes(RecipeTypeRegistry.ETHER_PROCESS_FACTORY_RECIPE.get());
         event.sendRecipes(RecipeTypeRegistry.NODE_PROCESS_RECIPE.get());
         EtherProcessRecipeManager.onReload(event.getPlayer().level().recipeAccess());
+        PacketDistributor.sendToPlayer(
+                (ServerPlayer) event.getPlayer(),
+                new SyncExtraRecipesS2C(EtherProcessRecipeManager.extraRecipes)
+        );
     }
 }
