1019c72d0430afd512edda9236f779992846a7ae
xypp <2952795729@qq.com>
2026-05-20 03:44:58 +0800

    feat: add ExtraRecipeProvider interface

---
+++ src/main/java/studio/fantasyit/ether_craft/factory/ExtraRecipeProvider.java
@@ -0,0 +1,15 @@
+package studio.fantasyit.ether_craft.factory;
+
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.level.ItemLike;
+
+import java.util.List;
+
+public interface ExtraRecipeProvider {
+    Identifier getCategoryId();
+
+    ItemLike getIcon();
+
+    List<EtherProcessRecipeManager.ExtraRecipe> generate(RecipeManager manager);
+}
