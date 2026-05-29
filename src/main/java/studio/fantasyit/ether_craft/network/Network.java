71e9d4ba06bf6cac0a6f608396c5bf0c682a2fc6
xypp <2952795729@qq.com>
2026-05-29 23:26:40 +0800

    修复一个网络包问题

--- src/main/java/studio/fantasyit/ether_craft/network/Network.java
+++ src/main/java/studio/fantasyit/ether_craft/network/Network.java
@@ -1,16 +1,20 @@
 package studio.fantasyit.ether_craft.network;
 
-import net.neoforged.api.distmarker.Dist;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.handling.IPayloadHandler;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+import org.jetbrains.annotations.NotNull;
+import studio.fantasyit.ether_craft.network.c2s.*;
 import studio.fantasyit.ether_craft.network.s2c.*;
 
-import static studio.fantasyit.ether_craft.network.NetworkClient.clientMsg;
+import java.util.function.BiConsumer;
 
 public class Network {
-    private static void commonMsg(PayloadRegistrar  event){
+    private static void commonMsg(PayloadRegistrar event) {
         event.playToClient(
                 SyncBlockEtherValueS2C.TYPE,
                 SyncBlockEtherValueS2C.CODEC,
@@ -56,24 +60,54 @@ public class Network {
                 EtherStreamUpdateS2C.CODEC,
                 EtherStreamUpdateS2C::handle
         );
+
+
+        event.playToServer(
+                TriggerSwitchTabC2S.TYPE,
+                TriggerSwitchTabC2S.CODEC,
+                wrapWithPlayer(TriggerSwitchTabC2SHandler::handle)
+        );
+        event.playToServer(
+                SetFilterSlotC2S.TYPE,
+                SetFilterSlotC2S.CODEC,
+                wrapWithPlayer(SetFilterSlotC2SHandler::handle)
+        );
+        event.playToServer(
+                SyncScreenDataC2S.TYPE,
+                SyncScreenDataC2S.CODEC,
+                wrapWithPlayer(SyncScreenDataC2SHandler::handle)
+        );
+        event.playToServer(
+                SyncFilterActiveC2S.TYPE,
+                SyncFilterActiveC2S.CODEC,
+                wrapWithPlayer(SyncFilterActiveC2SHandler::handle)
+        );
+        event.playToServer(
+                SetBlockNameC2S.TYPE,
+                SetBlockNameC2S.CODEC,
+                wrapWithPlayer(SetBlockNameC2SHandler::handle)
+        );
+        event.playToServer(
+                FactoryMenuSwitchItemC2S.TYPE,
+                FactoryMenuSwitchItemC2S.CODEC,
+                wrapWithPlayer(FactoryMenuSwitchItemC2SHandler::handle)
+        );
     }
 
 
     private static final String PROTOCOL_VERSION = "1";
-    @EventBusSubscriber(value = Dist.DEDICATED_SERVER)
-    public static class Server{
+
+    @EventBusSubscriber
+    public static class Server {
         @SubscribeEvent
         public static void register(RegisterPayloadHandlersEvent event) {
             commonMsg(event.registrar(PROTOCOL_VERSION));
         }
     }
-    @EventBusSubscriber(value = Dist.CLIENT)
-    public static class Client{
-        @SubscribeEvent
-        public static void register(RegisterPayloadHandlersEvent event) {
-            PayloadRegistrar r = event.registrar(PROTOCOL_VERSION);
-            commonMsg(r);
-            clientMsg(r);
-        }
+
+    private static <T extends CustomPacketPayload> IPayloadHandler<@NotNull T> wrapWithPlayer(BiConsumer<T, Player> t) {
+        return (d, c) -> {
+            c.enqueueWork(() -> t.accept(d, c.player()));
+        };
     }
 }
--- src/main/java/studio/fantasyit/ether_craft/network/NetworkClient.java
+++ src/main/java/studio/fantasyit/ether_craft/network/NetworkClient.java
@@ -12,41 +12,6 @@ import java.util.function.BiConsumer;
 
 public class NetworkClient {
     public static void clientMsg(PayloadRegistrar event) {
-        event.playToServer(
-                TriggerSwitchTabC2S.TYPE,
-                TriggerSwitchTabC2S.CODEC,
-                wrapWithPlayer(TriggerSwitchTabC2SHandler::handle)
-        );
-        event.playToServer(
-                SetFilterSlotC2S.TYPE,
-                SetFilterSlotC2S.CODEC,
-                wrapWithPlayer(SetFilterSlotC2SHandler::handle)
-        );
-        event.playToServer(
-                SyncScreenDataC2S.TYPE,
-                SyncScreenDataC2S.CODEC,
-                wrapWithPlayer(SyncScreenDataC2SHandler::handle)
-        );
-        event.playToServer(
-                SyncFilterActiveC2S.TYPE,
-                SyncFilterActiveC2S.CODEC,
-                wrapWithPlayer(SyncFilterActiveC2SHandler::handle)
-        );
-        event.playToServer(
-                SetBlockNameC2S.TYPE,
-                SetBlockNameC2S.CODEC,
-                wrapWithPlayer(SetBlockNameC2SHandler::handle)
-        );
-        event.playToServer(
-                FactoryMenuSwitchItemC2S.TYPE,
-                FactoryMenuSwitchItemC2S.CODEC,
-                wrapWithPlayer(FactoryMenuSwitchItemC2SHandler::handle)
-        );
     }
 
-    private static <T extends CustomPacketPayload> IPayloadHandler<@NotNull T> wrapWithPlayer(BiConsumer<T, Player> t) {
-        return (d, c) -> {
-            c.enqueueWork(() -> t.accept(d, c.player()));
-        };
-    }
 }
